0889bec16ccb73ebf50342a49c785c7682aded90
Dmitry <upssq@mail.ru>
2021-11-19 15:36:45 +0300

    Добавил валидацию формы

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,61 @@ const headerLinkSelect = body.querySelectorAll('.header__link-select-city');
 const headerButtonMenu = document.querySelector('.header__button-menu');
 const headeNav = body.querySelector('.header__wrapper-nav');
 const headerNavClose = body.querySelector('.header__nav-close');
+const modalForm = body.querySelector('.consist__form');
+const dataSabmitUrl = 'https://echo.htmlacademy.ru/';
+
+const getTemplateContent = (block, item) =>
+  block.querySelector(`#${item}`)
+    .content
+    .querySelector(`.${item}`);
+
+const success = getTemplateContent(body, 'alert__success');
+const errorLoading = getTemplateContent(body, 'alert__error-loading');
+
+const successElement = success.cloneNode(true);
+const successErrorLoading = errorLoading.cloneNode(true);
+
+const keys = {
+  escape: 'Escape',
+  esc: 'Escape',
+};
+
+const isEscEvent = (evt) => evt.key === keys.escape || evt.key === keys.esc;
+
+const onErrorRemove = () => {
+  successErrorLoading.remove();
+  document.removeEventListener('click', onErrorRemove);
+};
+
+const onSuccessRemove = () => {
+  successElement.remove();
+  document.removeEventListener('click', onSuccessRemove);
+};
+
+const onElementEscRemove = () => {
+  if (isEscEvent) {
+    onSuccessRemove();
+    document.removeEventListener('keydown', onElementEscRemove);
+  }
+};
+
+const onErrorEscRemove = () => {
+  if (isEscEvent) {
+    onErrorRemove();
+    document.removeEventListener('keydown', onErrorEscRemove);
+  }
+};
+
+const alertSuccess = () => {
+  body.append(successElement);
+  document.addEventListener('click', onSuccessRemove); document.addEventListener('keydown', onElementEscRemove);
+};
+
+const alertError = () => {
+  body.append(successErrorLoading);
+  document.addEventListener('click', onErrorRemove);
+  document.addEventListener('keydown', onErrorEscRemove);
+};
 
 const onClickSelectedMenu = () => {
   headerSelectCity.classList.toggle('header__select-city--active');
@@ -37,6 +92,32 @@ const onRemoveMenu = () => {
   headeNav.classList.remove('header__wrapper-nav--active');
 };
 
+const sendData = (url, bodyForm, alertSucces, error) => {
+  fetch(url, {
+    method: 'POST',
+    body: bodyForm,
+  })
+    .then((response) => {
+      if (response.ok) {
+        alertSucces();
+      } else {
+        error();
+      }
+    })
+    .catch(() => {
+      error();
+    });
+};
+
+const onFormSend = (evt) => {
+  evt.preventDefault();
+
+  const formData = new FormData(evt.target);
+
+  sendData(dataSabmitUrl, formData, alertSuccess, alertError);
+};
+
+modalForm.addEventListener('submit', onFormSend);
 headerButtonMenu.addEventListener('click', onAddMenu);
 headerNavClose.addEventListener('click', onRemoveMenu);
 headerSelectCity.addEventListener('click', onClickSelectedMenu);
